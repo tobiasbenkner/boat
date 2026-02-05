3bafc0c203fde887932d01668b962693652d4744
Tobias Benkner <kontakt@tobias-benkner.de>
2026-02-05 21:52:37 +0100

    add pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+const isGitHubPages = !!process.env.GITHUB_REPOSITORY;
+
 export default defineConfig({
+  site: isGitHubPages ? `https://boat.benkner-it.com` : 'http://localhost:4321',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
